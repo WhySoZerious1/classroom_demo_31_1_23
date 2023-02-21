017bf47dcdf1d050d7f7d76b726eb3df3824a6c7
WhySoZerious1 <124044396+WhySoZerious1@users.noreply.github.com>
2023-02-21 10:33:58 +0000

    Line Following

--- main.ts
+++ main.ts
@@ -1,20 +1,40 @@
+function Soft_Left () {
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 35)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 35)
+    basic.pause(100)
+}
+function Hard_Left () {
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 60)
+    basic.pause(100)
+}
+function AllAhead () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
+}
+function Soft_Right () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 35)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 35)
+    basic.pause(100)
+}
+function Hard_Right () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 60)
+    basic.pause(100)
+}
+let LFSR = 0
+let LFSL = 0
 let Intensity = 255
 basic.forever(function () {
-    basic.showLeds(`
-        . # . # .
-        . # . # .
-        # . . . #
-        . . # . .
-        . . # . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . # # # .
-        . # # # .
-        # . # . #
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(100)
-    Intensity += -10
+    LFSL = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
+    LFSR = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
+    if (LFSL == 1 && LFSR == 1) {
+        AllAhead()
+    } else if (LFSL == 1) {
+        Soft_Left()
+    } else if (LFSR == 1) {
+        Soft_Right()
+    } else {
+        AllAhead()
+    }
 })
