16ef11ca7f82e8cfa7bf63f745cf95ccfcc4b30d
WhySoZerious1 <124044396+WhySoZerious1@users.noreply.github.com>
2023-03-21 10:51:43 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 let avoiding_protocol = 0
 let LFSL = 0
 let LFSR = 0
+let Right = 0
 function Soft_Left () {
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 35)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 35)
-    basic.pause(100)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
 }
 function distance_to_stop () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 60)
@@ -16,29 +16,31 @@ function distance_to_stop () {
 function Hard_Left () {
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 60)
-    basic.pause(100)
+    basic.pause(500)
 }
 function AllAhead () {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
 }
 function Soft_Right () {
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 35)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 35)
-    basic.pause(100)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
 }
 function Hard_Right () {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 60)
-    basic.pause(100)
+    basic.pause(500)
 }
 function Avoid () {
+    avoiding_protocol = 1
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 60)
-    basic.pause(100)
+    basic.pause(500)
     Hard_Right()
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 60)
+    basic.pause(200)
     while (avoiding_protocol == 1) {
-        if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
-            Soft_Right()
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) < 20) {
+            Hard_Right()
         } else {
             Soft_Left()
         }
@@ -55,11 +57,17 @@ basic.forever(function () {
     LFSL = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
     LFSR = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
     if (LFSL == 1 && LFSR == 1) {
-        AllAhead()
+        if (Right) {
+            Soft_Left()
+        } else {
+            Soft_Right()
+        }
     } else if (LFSL == 1) {
-        Soft_Left()
-    } else if (LFSR == 1) {
         Soft_Right()
+        Right = 1
+    } else if (LFSR == 1) {
+        Soft_Left()
+        Right = 0
     } else {
         AllAhead()
     }
